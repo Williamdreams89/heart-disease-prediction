688d077f0b44725dbb5c913097014ff76dc6a1c7
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2024-01-14 23:48:02 +0000

    parsing the inputs as numbers

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ const App = () => {
   const handleInputChange = (name: string, value: string): void => {
     setFormData((prevData) => ({
       ...prevData,
-      [name]: value,
+      [name]: parseInt(value),
     }));
   };
 
